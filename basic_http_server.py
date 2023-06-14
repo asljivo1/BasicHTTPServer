32b4ab76f920c8799cec0aabde7bb57198d32fde
Amina Seferagic <ami.sljivo@gmail.com>
2023-06-14 16:52:29 +0200

    Use GitHub OAuth App for authentification

--- basic_http_server.py
+++ basic_http_server.py
@@ -1,49 +1,59 @@
 import http.server
 import socketserver
 import base64
-import hashlib
-import configparser
+import webbrowser
+import urllib.parse
+import requests
+import json
 
 PORT = 8080
+CLIENT_ID = '94e7c973ecaeb3068218'
+CLIENT_SECRET = '279a8158f650e7b980b3bb2df11fe5fceb1061da'
 
 class CustomHandler(http.server.SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        self.config = configparser.ConfigParser()
-        self.config.read('config.ini')
-
-        super().__init__(*args, **kwargs)
-
     def do_GET(self):
-        #Basic authentication
         if self.path == '/token':
-            # Check if the request has the Authorization header
-            if 'Authorization' in self.headers and self.headers['Authorization'].startswith('Basic '):
-                # Decode basic auth creds
-                auth_type, auth_token = self.headers['Authorization'].split(' ')
-                decoded_credentials = base64.b64decode(auth_token).decode('utf-8')
-                read_client_id, read_client_secret = decoded_credentials.split(':')
+            # OAuth authentication logic
+            # Retrieve the authorisation code and exchange it for access token
+            auth_url = f'https://github.com/login/oauth/authorize?client_id={CLIENT_ID}&scope=user'
+            webbrowser.open(auth_url)
 
-                # Hash the client secret
-                hashed_read_client_secret = hashlib.sha256(read_client_secret.encode()).hexdigest()
-                myid = self.config.get('Credentials', 'client_id')
-                mysecret = self.config.get('Credentials', 'hashed_client_secret')
+        if self.path.startswith('/callback'):
+            # Parse the authorisation code for the callback URL
+            query = urllib.parse.urlparse(self.path).query
+            params = urllib.parse.parse_qs(query)
+            code = params['code'][0]
+            
+            # Exchange the authorisation code for an access token
+            token_url = 'https://github.com/login/oauth/access_token'
+            payload = {
+                'client_id': CLIENT_ID,
+                'client_secret': CLIENT_SECRET,
+                'code': code
+            }
+            headers = {'Accept': 'application/json'}
+            response = requests.post(token_url, data=payload, headers=headers)
 
-                if read_client_id == myid and hashed_read_client_secret == mysecret:
-                    self.send_response(200)
-                    self.send_header('Content-type', 'text/plain')
-                    self.end_headers()
+            data = response.json()
+            if 'access_token' in data:
+                access_token = response.json()['access_token']
+
+                # Check if we were granted user scope
+                #scopes = response.json()['scope'].split(',')
+                #has_user_email_scope = 'user' in scopes
+
+                user_url = 'https://api.github.com/user'
+                headers = {'Accept': 'application/vnd.github+json','Authorization': f"Bearer {access_token}"} #vnd.github+json   , "X-GitHub-Api-Version": "2022-11-28"
+                response = requests.get(user_url, headers = headers)
+                if response.status_code == 200:
+                    user_data = response.json()
                     self.wfile.write(b'Authentication successful!')
                     return
-
-            # Authentication failed
-            self.send_response(401)
-            self.send_header('WWW-Authenticate', 'Basic realm="Authentication required"')
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write(b'Authentication failed!')
+                else:
+                    print(f"Error: {response.json().get('message', 'unnknown error')}")
         else:
-            # Handle other endpoints with the default behavior
             super().do_GET()
+
 
 with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
     print("Server running at http://localhost:{}".format(PORT))
